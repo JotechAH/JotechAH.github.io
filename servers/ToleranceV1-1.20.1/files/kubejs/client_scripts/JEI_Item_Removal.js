855acf1029b38cd80ca8af74a7f2b9d74336e6c7
JotechAH <47044295+JotechAH@users.noreply.github.com>
2025-09-20 22:02:12 +0200

    Redoing from scratch

--- servers/ToleranceV1-1.20.1/files/kubejs/client_scripts/JEI_Item_Removal.js
+++
@@ -1,9 +0,0 @@
-JEIEvents.hideItems(event => {
-	event.hide([
-		"apotheosis:augmenting_table",
-        "apotheosis:simple_reforging_table",
-        "apotheosis:reforging_table",
-        "apotheosis:sigil_of_rebirth",
-        "apotheosis:sigil_of_enhancement"
-	]);
-})
--- servers/ToleranceV1-1.20.1/files/kubejs/startup_scripts/Currency.js
+++
@@ -1,7 +0,0 @@
-StartupEvents.registry('item', event => {
-  event.create('tolecoin')
-    .displayName('§6ToleCoin')
-    .tooltip('La monnaie officielle du serveur')
-    .rarity('epic')
-    .texture('apotheotic_additions:item/ascended_coin')
-})
